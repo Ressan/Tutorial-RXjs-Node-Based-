6d0a9ee52673e894cd37421888b147065edd923e
Ressan <46036784+Ressan@users.noreply.github.com>
2022-04-28 16:59:35 +0200

    Trying to merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,10 @@
-import { Component, OnInit, OnDestroy, VERSION } from '@angular/core';
-import { Observable, interval, Subscription, of, from, Subscriber } from 'rxjs';
-import { map, tap, take, filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: []
 })
-export class AppComponent implements OnInit, OnDestroy
-{
-  private subscription: Subscription = new Subscription();
-  ngOnInit() {
-    function multiplier(item: number): number{
-      return 2 * item;
-    }
-  
-    from([1, 0, 12,0, undefined, 13,0,14,15]).pipe(
-      // tap(elem => console.log(elem)),
-      // map((elem : number) =>{
-      //   if (elem == 0)
-      //   {
-      //     throw new Error('Erreur 0')
-      //   }
-      //   return elem * 2;
-      // }),
-      // map(item => item - 2),
-      // take(2)
-      filter(elem => elem != undefined && elem != 0)
-     ).subscribe(
-        (item: number | undefined) => console.log(`ma valeur ${item}`),
-        (err: unknown) => console.error(err),
-        () => console.log('Fin from !')
-      )
-  }
-
-  public start(): void {
-    this.subscription.add(interval(1000).subscribe(
-      (value) => console.log('ma valeur', value),
-      (error) => console.error(error),
-      () => console.log('terminé')
-    ));
-
-    this.subscription.add(interval(1000).subscribe(
-      (value) => console.warn(' == ma valeur==: ', value),
-      (error) => console.error(error),
-      () => console.log(' == terminé ==')
-    ));
-
-  }
-
-  public stop(): void {
-    this.subscription.unsubscribe();
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
+export class AppComponent {
+  title = 'Gestionnaire hotel';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
-import { NgModule,  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HotelModule } from './hotels/hotel.module';
+import { AppRoutingModule } from './app-routing.module';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    HotelModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
