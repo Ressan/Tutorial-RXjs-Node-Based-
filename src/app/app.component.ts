61db1a6b90424de7820e00848fe713e45e85fd8b
Ressan <ressancoding@gmail.com>
2022-04-28 14:59:46 +0200

    Operateur - map tap take filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, VERSION } from '@angular/core';
 import { Observable, interval, Subscription, of, from, Subscriber } from 'rxjs';
+import { map, tap, take, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,60 +10,29 @@ import { Observable, interval, Subscription, of, from, Subscriber } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy
 {
   private subscription: Subscription = new Subscription();
-
   ngOnInit() {
-    const observer = {
-      next: (item: unknown) => console.log(`Une boîte arrive ${item}`),
-      error: (err: unknown) => console.log(`Oups, erreur : ${err}`),
-      complete: () => console.log('Fin de stream!'),
-    };
-
-    const stream = new Observable((myObserver) => {
-      myObserver.next('Boite 1');
-      myObserver.error(new Error());
-      myObserver.next('Boite 2');
-      myObserver.next('Boite 3');
-      // for(let index = 0; index < 100; index ++)
-      // {
-      //   myObserver.next('Boite');
-      // }
-      myObserver.complete();
-      myObserver.next('Boite 3');
-    });
-
-    //stream.subscribe(observer);
-    /* Depracted method : */
-    const subscription = stream.subscribe(
-      (item) => console.log(`Une boîte arrive ${item}`),
-      (err) => console.log(`Oups, erreur ${err}`),
-      () => console.log('Fin de stream!')
-    );
-
-    subscription.unsubscribe();
-
-    // from([12,13,14,15]).subscribe(
-      //   (item: number) => console.log(`ma valeur ${item}`),
-      //   (err: unknown) => console.error(err),
-      //   () => console.log('Fin from !')
-      // )
-      
-      const double = (source: Observable<number>) => new Observable<number>((subscriber) => {
-        const subscription = source.subscribe({
-          next: (value) => subscriber.next(2 * value),
-          error: (err) => subscriber.error(err),
-          complete : () => subscriber.complete()
-        })
-
-        return () => {
-          subscription.unsubscribe();
-        }
-      })
-      
-      of(1,2,3,4).pipe(
-        double,
-        double
-      ).subscribe(console.log); 
+    function multiplier(item: number): number{
+      return 2 * item;
     }
+  
+    from([1, 0, 12,0, undefined, 13,0,14,15]).pipe(
+      // tap(elem => console.log(elem)),
+      // map((elem : number) =>{
+      //   if (elem == 0)
+      //   {
+      //     throw new Error('Erreur 0')
+      //   }
+      //   return elem * 2;
+      // }),
+      // map(item => item - 2),
+      // take(2)
+      filter(elem => elem != undefined && elem != 0)
+     ).subscribe(
+        (item: number | undefined) => console.log(`ma valeur ${item}`),
+        (err: unknown) => console.error(err),
+        () => console.log('Fin from !')
+      )
+  }
 
   public start(): void {
     this.subscription.add(interval(1000).subscribe(
