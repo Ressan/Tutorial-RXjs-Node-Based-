6e9e16822bd0d468e62381a331543267a0111eeb
Ressan <ressancoding@gmail.com>
2022-04-28 14:11:33 +0200

    Opérateur - création double

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy, VERSION } from '@angular/core';
+import { Observable, interval, Subscription, of, from, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit, OnDestroy
+{
+  private subscription: Subscription = new Subscription();
+
   ngOnInit() {
     const observer = {
       next: (item: unknown) => console.log(`Une boîte arrive ${item}`),
@@ -37,5 +39,52 @@ export class AppComponent {
     );
 
     subscription.unsubscribe();
+
+    // from([12,13,14,15]).subscribe(
+      //   (item: number) => console.log(`ma valeur ${item}`),
+      //   (err: unknown) => console.error(err),
+      //   () => console.log('Fin from !')
+      // )
+      
+      const double = (source: Observable<number>) => new Observable<number>((subscriber) => {
+        const subscription = source.subscribe({
+          next: (value) => subscriber.next(2 * value),
+          error: (err) => subscriber.error(err),
+          complete : () => subscriber.complete()
+        })
+
+        return () => {
+          subscription.unsubscribe();
+        }
+      })
+      
+      of(1,2,3,4).pipe(
+        double,
+        double
+      ).subscribe(console.log); 
+    }
+
+  public start(): void {
+    this.subscription.add(interval(1000).subscribe(
+      (value) => console.log('ma valeur', value),
+      (error) => console.error(error),
+      () => console.log('terminé')
+    ));
+
+    this.subscription.add(interval(1000).subscribe(
+      (value) => console.warn(' == ma valeur==: ', value),
+      (error) => console.error(error),
+      () => console.log(' == terminé ==')
+    ));
+
   }
+
+  public stop(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 }
